aca5a59b554f91aa50f3d979b77ae1b425347d57
Jeferson de Sousa Coutinho <jeferson_sousa@yahoo.com.br>
2023-05-23 20:02:30 -0300

    Commit initial

---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import { getUser } from "@/lib/auth";
+import Link from "next/link";
+import Image from "next/image";
+
+export function Profile() {
+  const { name, avatarUrl } = getUser();
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
+          Quero sair
+        </a>
+      </p>
+    </div>
+  );
+}
